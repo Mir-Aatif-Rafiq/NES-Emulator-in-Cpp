f7d83c92621381a263d9db762d9a64b979df8987
Mir-Aatif-Rafiq <bitemiraatif@gmail.com>
2024-08-28 11:56:48 +0530

    [BUILD] cpu.cpp : complete function for addr modes

--- cpu.cpp
+++ cpu.cpp
@@ -47,17 +47,20 @@ and then we run the opcode function and increment pc along the way
 and repeat.
 
 */
-u_int8_t CPU6502::IMP() {  // Implied is just one byte instructions
+bool CPU6502::IMP() {  // Implied is just one byte instructions
 	PC++;
+	return 0;
 
 }
 
-u_int8_t CPU6502::IMM() {   // in assembly LDA  #$42  , uses # before value, loads A with hex val 42
+bool CPU6502::IMM() {   // in assembly LDA  #$42  , uses # before value, loads A with hex val 42
 														// depending on the addressing mode of LDA it will have differernt opcodes.	
 														// clock cycles of the whole instruction regardless of opcode = 2 
 	data_fetched = bus->bus_read(PC + 1); // now this data _fetched can be directly accessed by the opcode 
 																				// bc its the part of the class.
 	PC += 2;	
+
+	return 0;
 
 	// clearing of the data_fetched is the responsibility of the opcode
 
@@ -67,47 +70,78 @@ u_int8_t CPU6502::IMM() {   // in assembly LDA  #$42  , uses # before value, loa
 /*
 * in 6502 the pages are indexed using the upper byte and the offset is the lower byte.
 */
-u_int8_t CPU6502::ZP0() {  // its just the first page. operand is offeset of ZP
+bool CPU6502::ZP0() {  // its just the first page. operand is offeset of ZP
 
 	abs_addr_fetched = 0x0000 + bus->bus_read(PC + 1); // 8bit is extended to 16bit without msb extention
 	data_fetched = bus->bus_read(abs_addr_fetched);
  	PC+=2;
+	return 0;
 }
-u_int8_t CPU6502::ZPX() {
+bool CPU6502::ZPX() {
 	// if page crossed theres a wrap around condition within the zeroth page
-	if( X + bus->bus_read(PC + 1) > 0xFF){
-		abs_addr_fetched =(X + bus->bus_read(PC + 1)) & 0x00FF;  // addition of 0xff+0xff will still be in 1st page maths checks out
-	}
-	else {
-		abs_addr_fetched = X + 0x0000 + bus->bus_read(PC + 1); 
-	}
+	// addition of 0xff+0xff will still be in 1st page maths checks out
+
+	abs_addr_fetched =(X + bus->bus_read(PC + 1)) & 0x00FF; // &0xFF handle wrap around
 	data_fetched = bus->bus_read(abs_addr_fetched);
 	
 	PC+=2;
+	return 0;
 }  // its just the first page
 
 // i think remove if and & with 0xFF regardless of the situation''''''''''''''
-u_int8_t CPU6502::ZPY() {
-	if( Y + bus->bus_read(PC + 1) > 0xFF){
-		abs_addr_fetched =(Y + bus->bus_read(PC + 1)) -  0x0100;  // addition of 0xff+0xff will still be in 1st page maths checks out
-	}
-	else{
-		abs_addr_fetched = Y + 0x0000 + bus->bus_read(PC + 1); 
-	}
+bool CPU6502::ZPY() {
+
+	abs_addr_fetched =(Y + bus->bus_read(PC + 1)) & 0x00FF;
 	data_fetched = bus->bus_read(abs_addr_fetched);
 	
 	PC+=2;
+	return 0;
 }
 // ZPX and ZPY are zero page indexed with contents of X and Y register
 
 // similarly with ABX and ABY , some address then offset with X and Y reg
 
-u_int8_t CPU6502::ABS() { // e.x. LDA $8080
+bool CPU6502::ABS() { // e.x. LDA $8080
+// in ABS if the lo byte is 0xXXFF the high byte will be taken from 
+// 0xX(X+1)FF noramlly crossing page boundary without 
+// triggering extra clock cycle
 	lo_data_fetched = bus->bus_read(PC + 1);
 	hi_data_fecthed = bus->bus_read(PC + 2);
 	abs_addr_fetched = (hi_data_fecthed << 8) | lo_data_fetched;// yeah it works(shifting) without making hi_data_fetched 16 bit
 	data_fetched = bus->bus_read(abs_addr_fetched);
 	PC += 3;
+
+	return 0;
+}
+bool CPU6502::ABX() {
+	lo_data_fetched = bus->bus_read(PC + 1);
+	hi_data_fecthed = bus->bus_read(PC + 2);
+	abs_addr_fetched = (hi_data_fecthed << 8) | lo_data_fetched;
+	abs_addr_fetched += X;
+	data_fetched = bus->bus_read(abs_addr_fetched);
+	PC += 3;
+	if((abs_addr_fetched & 0xFF00) != (hi_data_fecthed<<8)){
+		// means page boundary has been crossed so addition clock cycle
+		return 1;
+	}
+	else
+	return 0;
+}
+
+bool CPU6502::ABY() {
+	lo_data_fetched = bus->bus_read(PC + 1);
+	hi_data_fecthed = bus->bus_read(PC + 2);
+	abs_addr_fetched = (hi_data_fecthed << 8) | lo_data_fetched;
+	abs_addr_fetched += Y;
+	data_fetched = bus->bus_read(abs_addr_fetched);
+	PC += 3;
+	if((abs_addr_fetched & 0xFF00) != (hi_data_fecthed<<8)){
+		// means page boundary has been crossed so addition clock cycle
+		return 1;
+	}
+	else 
+	return 0;
+
 }
 
 // Note: The next 3 address modes use indirection (aka Pointers!)
@@ -122,7 +156,7 @@ u_int8_t CPU6502::ABS() { // e.x. LDA $8080
 * invalid actual address
 */ 
 
-u_int8_t CPU6502::IND() { // indirect mode is pointer only for jumps
+bool CPU6502::IND() { // indirect mode is pointer only for jumps
 																	//  jmp ($2000) : jumps to the location pointed by 
 																	// values of address $2000 and $2001. with val of 2001 being upper byte
 	lo_data_fetched = bus->bus_read(PC + 1);
@@ -141,12 +175,15 @@ u_int8_t CPU6502::IND() { // indirect mode is pointer only for jumps
 	abs_addr_fetched = (hi_data_fecthed << 8) | lo_data_fetched;
 
 	data_fetched = bus->bus_read(abs_addr_fetched); // i dont think we have to fetch anything?
+	PC += 3;
 
+	return 0;
 }   
 
 
 // i think here we also have to tackle the conditn of lo_byte+X being 0xFF
-u_int8_t CPU6502::IZX() {  // (zero page offset + x reg) value as pointer.
+// SOLVED : it wraps around
+bool CPU6502::IZX() {  // (zero page offset + x reg) value as pointer.
 																		// adding first and then dereference
 
 																		// e.g. LDA ($70,X)
@@ -155,17 +192,27 @@ u_int8_t CPU6502::IZX() {  // (zero page offset + x reg) value as pointer.
 																					 // even if wrap around happens,
 																					 // it will get handled
 
+  if(rel_addr_fetched & 0xFF == 0xFF){ // trigger CPU bug of wrap around
+		lo_data_fetched = bus->bus_read(rel_addr_fetched);
+		hi_data_fecthed = bus->bus_read(rel_addr_fetched & 0xFF00);
+	}
 
-	lo_data_fetched = bus->bus_read(rel_addr_fetched);
-	hi_data_fecthed = bus->bus_read(rel_addr_fetched + 1);
+	else {
+		lo_data_fetched = bus->bus_read(rel_addr_fetched);
+		hi_data_fecthed = bus->bus_read(rel_addr_fetched + 1);
+	}
 
 	abs_addr_fetched = (hi_data_fecthed << 8) | lo_data_fetched;
 	
 	data_fetched = bus->bus_read(abs_addr_fetched);
 
+	PC += 2;
+
+	return 0;
+
 }
 
-u_int8_t  CPU6502::IZY() { // Post indexed mode first derefernce than add Y 
+bool  CPU6502::IZY() { // Post indexed mode first derefernce than add Y 
 																		// only works with Y reg
 																		// e.g. LDA ($79),Y
 	rel_addr_fetched = 0x0000 + bus->bus_read(PC + 1);
@@ -178,12 +225,20 @@ u_int8_t  CPU6502::IZY() { // Post indexed mode first derefernce than add Y
 
 	abs_addr_fetched = (hi_data_fecthed << 8) | lo_data_fetched;
 	abs_addr_fetched += Y;
-	
+
 	data_fetched = bus->bus_read(abs_addr_fetched);
+	
+	PC+=2;
+
+	if((abs_addr_fetched & 0xFF00) != (hi_data_fecthed<<8)){ // boundary crossed
+		return 1;
+	}
+	else
+	return 0;
 
 }
 
-u_int8_t  CPU6502::REL() { // relative wrt PC but signed 2s compliment 
+bool  CPU6502::REL() { // relative wrt PC but signed 2s compliment 
 																		// max branch possible 127 bytes
 																		// remember to minus the PC -1 before adding offset.
 	lo_data_fetched = bus->bus_read(PC + 1);
@@ -198,6 +253,8 @@ u_int8_t  CPU6502::REL() { // relative wrt PC but signed 2s compliment
 	else {
 		rel_addr_fetched = (PC + 1 + lo_data_fetched) & 0x00FF;
 	}
+	PC +=2;
+	return 0;
 }
 
 /*
